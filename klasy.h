3c13511f17923f6571724c404ccded2f6556e177
BoyZel <mateuszrasmus@protonmail.ch>
2018-10-20 10:57:08 +0200

    Functions added

--- klasy.h
+++ klasy.h
@@ -7,16 +7,16 @@ class Mariusz{
 	int strenght;
 	int agility;
 	int endurance;
-	void insert(int a, int b,int c);
+	void insert(string s,int a, int b,int c);
 	int power();
 };
 class Jaroslaw{
 	public:
 	string surname;
 	int proc_timing;
-	int speed_of_connection;
+	int connection_speed;
 	int programming_skills;
-	void insert(int a,int b,int c);
+	void insert(string s,int a,int b,int c);
 	int power();
 };
 class Zbyszek{
@@ -25,6 +25,6 @@ class Zbyszek{
 	int horsepower;
 	int driving_skills;
 	int alcohol_promiles;
-	void insert(int a,int b,int c);
+	void insert(string s,int a,int b,int c);
 	int power();
 };
