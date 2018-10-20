ec06c1b7824b98887b3408e0d58e131b517508be
BoyZel <mateuszrasmus@protonmail.ch>
2018-10-20 10:41:49 +0200

    correct extension

---
+++ klasy.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Mariusz{
+	public:
+	string surname;
+	int strenght;
+	int agility;
+	int endurance;
+	void insert(int a, int b,int c);
+	int power();
+};
+class Jaroslaw{
+	public:
+	string surname;
+	int proc_timing;
+	int speed_of_connection;
+	int programming_skills;
+	void insert(int a,int b,int c);
+	int power();
+};
+class Zbyszek{
+	public:
+	string surname;
+	int horsepower;
+	int driving_skills;
+	int alcohol_promiles;
+	void insert(int a,int b,int c);
+	int power();
+};
